ce91ef6501d09bdad67804cee2feab6a0e3ce25c
egeltje <github@ewak.net>
2018-06-26 11:29:23 +0200

    Added code for generating individual flames on all 8 octows2811 pins

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,21 @@
 
 
 void setup() {
-  delay(3000); // sanity delay
-  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.setBrightness( BRIGHTNESS );
-
   pinMode(13, OUTPUT);      // TEST: turn LED on to show active program
   digitalWrite(13, HIGH);   // TEST: turn LED on to show active program
+
+  delay(1000); // sanity delay
+  FastLED.addLeds<CHIPSET, LED_PIN0, COLOR_ORDER>(leds[0], NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<CHIPSET, LED_PIN1, COLOR_ORDER>(leds[1], NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<CHIPSET, LED_PIN2, COLOR_ORDER>(leds[2], NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<CHIPSET, LED_PIN3, COLOR_ORDER>(leds[3], NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<CHIPSET, LED_PIN4, COLOR_ORDER>(leds[4], NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<CHIPSET, LED_PIN5, COLOR_ORDER>(leds[5], NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<CHIPSET, LED_PIN6, COLOR_ORDER>(leds[6], NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<CHIPSET, LED_PIN7, COLOR_ORDER>(leds[7], NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness( BRIGHTNESS );
+
+  digitalWrite(13, LOW);   // TEST: turn LED on to show active program
 }
 
 void loop()
@@ -20,7 +29,7 @@ void loop()
   Fire2012(); // run simulation frame
 
   FastLED.show(); // display this frame
-  FastLED.delay(1000 / FRAMES_PER_SECOND);
+  FastLED.delay(1000 / FRAMES_PER_SECOND / NUM_STRIPS);
 }
 
 void Fire2012()
@@ -28,24 +37,27 @@ void Fire2012()
   // Fire2012 by Mark Kriegsman, July 2012
   // as part of "Five Elements" shown here: http://youtu.be/knWiGsmgycY
 
-  static byte heat[NUM_LEDS];
+  static byte heat[NUM_STRIPS][NUM_LEDS];
+  int i, j;
 
-  // Step 1.  Cool down every cell a little
-    for( int i = 0; i < NUM_LEDS; i++) {
-      heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+  for (i = 0; i < NUM_STRIPS; i++) {
+    // Step 1.  Cool down every cell a little
+    for(j = 0; j < NUM_LEDS; j++) {
+      heat[i][j] = qsub8( heat[i][j],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
     }
-  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-    for( int k= NUM_LEDS - 1; k >= 2; k--) {
-      heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+    for(j= NUM_LEDS - 1; j >= 2; j--) {
+      heat[i][j] = (heat[i][j - 1] + heat[i][j - 2] + heat[i][j - 2] ) / 3;
     }
-  // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
     if( random8() < SPARKING ) {
-      int y = random8(7);
-      heat[y] = qadd8( heat[y], random8(160,255) );
+      j = random8(7);
+      heat[i][j] = qadd8( heat[i][j], random8(160,255) );
     }
-  // Step 4.  Map from heat cells to LED colors
+    // Step 4.  Map from heat cells to LED colors
     for( int j = 0; j < NUM_LEDS; j++) {
-      CRGB color = HeatColor(heat[j]);
-      leds[j] = color;
+      CRGB color = HeatColor(heat[i][j]);
+      leds[i][j] = color;
     }
+  }
 }
--- src/main.h
+++ src/main.h
@@ -1,16 +1,23 @@
-#define LED_PIN           2
+#define LED_PIN0          2
+#define LED_PIN1          14
+#define LED_PIN2          7
+#define LED_PIN3          8
+#define LED_PIN4          6
+#define LED_PIN5          20
+#define LED_PIN6          21
+#define LED_PIN7          5
 #define COLOR_ORDER       GRB
 #define CHIPSET           WS2812
 #define NUM_LEDS          60
+#define NUM_STRIPS        8
 
-#define BRIGHTNESS        200
+#define BRIGHTNESS        127
 #define FRAMES_PER_SECOND 60
 
 // For Fire2012
-#define COOLING  55
-#define SPARKING 120
+#define COOLING  75       // 55
+#define SPARKING 120      // 120
 
-
-CRGB leds[NUM_LEDS];
+CRGB leds[NUM_STRIPS][NUM_LEDS];
 
 void Fire2012();
